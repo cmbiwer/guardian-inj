637aa71e755216334744e142f732ca23a48d578a
Christopher Biwer <cmbiwer@gmail.com>
2016-02-03 13:53:21 -0600

    Add module for using awg to inject signal.

---
+++ lib/injawg.py
@@ -0,0 +1,12 @@
+from awg import ArbitraryLoop
+
+def awg_inject(channel_name, timeseries, gps_start_time,
+               sample_rate, ramptime=0):
+    """ None.
+    """
+
+    awg_exc = ArbitraryLoop(chan, timeseries, scale=scale_factor,
+                            rate=sample_rate, start=gps_start_time)
+    awg_exc.start(ramptime=ramptime, wait=True)
+    awg_exc.stop()
+
