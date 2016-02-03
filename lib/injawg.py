d1498dd8a92a12c9b09620818b81cc3d810b1ce4
Christopher Biwer <cmbiwer@gmail.com>
2016-02-03 15:18:33 -0600

    Doc string.

--- lib/injawg.py
+++ lib/injawg.py
@@ -9,14 +9,36 @@ This module calls awg to inject a signal.
 from awg import ArbitraryLoop
 
 def awg_inject(channel_name, timeseries, gps_start_time,
-               sample_rate, scale_factor=1.0, ramptime=0):
-    """ None.
+               sample_rate, scale_factor=1.0, ramp_time=0):
+    """ Injects a time series into channel.
+
+    Parameters
+    ----------
+    channel_name: str
+        Name of excitation channel to inject signal.
+    timeseries: numpy.array
+        Array with the time series to be injected.
+    gps_start_time: float
+        GPS start time of the injection.
+    sample_rate: int
+        Sample rate of the time series and excitation channel.
+    scale_factor: float
+        Factor that will be multipled to the time series before injection.
+    ramp_time: str
+        Time to ramp up the signal.
+
+    Returns
+    ----------
+    retcode: int
+        The exit code of injecting. 0 should mean successful and non-zero
+        should mean failure.
     """
 
     # call awg and inject the time series
     awg_exc = ArbitraryLoop(channel_name, timeseries, scale=scale_factor,
                             rate=sample_rate, start=gps_start_time)
-    awg_exc.start(ramptime=ramptime, wait=True)
+    awg_exc.start(ramptime=ramp_time, wait=True)
     awg_exc.stop()
+    retcode = 0
 
-    return 0
+    return retcode
