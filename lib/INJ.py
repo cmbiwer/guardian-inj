ef3339497aa132cb878eb24772ba7bc346db464f
Christopher Biwer <cmbiwer@gmail.com>
2016-02-03 12:55:23 -0600

    Update INJ guardian to new organization.

--- lib/INJ.py
+++ lib/INJ.py
@@ -1,176 +1,100 @@
 # -*- mode: python; tab-width: 4; indent-tabs-mode: nil -*-
 
-'''
+"""
 INJ base guardian module
 
 This defines the behavior for all transient injections.
-'''
+"""
 
-###############################################################################
-# IMPORTS
-###############################################################################
-
-import injtools
-#import injupload
-from guardian import GuardState
 from gpstime import gpstime
-
-###############################################################################
-# VARIABLES
-###############################################################################
+from guardian import GuardState
+from injtools import check_exttrig_alert, check_imminent_injection, read_schedule
 
 # name of channel to inject transient signals
-exc_channel = 'CAL-INJ_TRANSIENT_EXC'
+model_name = "CAL-INJ"
+exc_channel_name = model_name + "_TRANSIENT_EXC"
+
+# name of channel to check for external alerts
+exttrig_channel_name = model_name + "_EXTTRIG_ALERT_TIME"
+
+# seconds to wait for an external alert
+exttrig_wait_time = 3600
+
+# seconds to check for an imminent hardware injection
+imminent_wait_time = 60
 
 # path to schedule file
-schedule_path = 'fake_schedule'
+schedule_path = "fake_schedule"
 
 # sample rate of excitation channel and waveform files
 sample_rate = 16384
 
-#FIXME: use echo for development but for production should be awgstream
-# executable to perform hardware injections
-inj_exe = 'echo'
-
-# global variable for imminent injection
-imminent_inj = None
-
-###############################################################################
-# STATES
-###############################################################################
+# global variable for imminent HardwareInjection
+global imminent_hwinj
 
 class INIT(GuardState):
-    ''' The INIT state is the first state entered when starting the Guardian
-    daemon. It will run INIT.main once and then there will be a jump transition
+    """ The INIT state is the first state entered when starting the Guardian
+    daemon. It will run INIT.main once where then there will be a jump transition
     to the DISABLED state.
-    '''
+    """
 
     def main(self):
-        ''' Executate method once.
-        '''
+        """ Execute method once.
+        """
 
-        return 'DISABLED'
+        return "DISABLED"
 
 class DISABLED(GuardState):
-    ''' The DISABLED state is for when injections have been disabled manually
-    or if there is an electromagnetic alert. The DISABLED state will loop
-    continuously checking if injections have been enabled and enough time has
-    passed since the last electromagnetic alert. If this is true, then there
-    will be a jump transition to the IDLE state.
-    '''
+    """ The DISABLED state is for when injections have been disabled manually.
+    The DISABLED state will not be left unless the operator requests.
+    """
 
     # automatically assign edges from every other state
     goto = True
 
     def run(self):
-        ''' Execute method in a loop.
-        '''
+        """ Execute method in a loop.
+        """
 
-        #! FIXME: this is assuming old setup of a TINJ_ENABLE button
-        #! could operator request change on guardian interface
-        # if injections are enabled then move to IDLE state
-        inj_enabled = injtools.check_injections_enabled()
-        if inj_enabled:
-            return 'IDLE'
+        #! FIXME: for tests only
+        return "ENABLED"
 
-class IDLE(GuardState):
-    ''' The IDLE state will loop continuously. In each loop it will start off
-    by reading the schedule and checking if there is an imminent injection.
+        return
 
-    There is a check to see if injections have been disabled. If injections
-    have been disabled then there will be a jump transition to the DISABLED
+class ENABLED(GuardState):
+    """ The ENABLED sate is the state that is requested when moving out of
+    the DISABLED state. The ENABLED state will jump transition to the IDLE
     state.
-
-    Now there are two cases.
-
-    The first case injections are enabled and there is an imminent injection,
-    then there is a check to see if the detector is in observation mode. If
-    the detector is in observation mode then there is a jump transition to the
-    injection-type state, eg. CBC, BURST, or STOCHASTIC states. Else the
-    IDLE.run loop is repeated.
-
-    The second case there is no imminent injection and the IDLE.run loop is
-    repeated.
-    '''
-
-    def run(self):
-        ''' Execute method in a loop.
-        '''
-
-        # read schedule
-        inj_list = injtools.read_schedule(schedule_path)
-        log('There are %d injections in the future'%len(inj_list))
-
-        # check if injection is imminent
-        global imminent_inj
-        imminent_inj = injtools.check_injections_imminent(inj_list)
-
-        # if injections are disabled then move to DISABLED state
-        inj_enabled = injtools.check_injections_enabled()
-        if not inj_enabled:
-            log('Injections have been disabled')
-            return 'DISABLED'
-
-        # if there is an imminent injection then check if detector is observing
-        if imminent_inj:
-            log('There is an imminent injection at %f'%imminent_inj.scheduled_time)
-            detector_enabled = injtools.check_detector_enabled()
-
-            # if detector is in observation mode then change to
-            # injection-type state, eg. CBC, BURST, or STOCHASTIC
-            if detector_enabled:
-                return imminent_inj.inj_type
-
-            # else continue in IDLE.run and do not perform injection
-            else:
-                log('Will not perform injection because detector is' + \
-                         'not in observation mode')
-
-        # else there is no imminent injection continue
-        else:
-            log('There is no imminent injection')
-
-class CBC(GuardState):
-    ''' The CBC state is for performing CBC hardware injections. In the CBC
-    state a hardware injection event is uploaded to GraceDB. Then an external
-    call to awgstream is used to inject the waveform into the detector.
-    '''
+    """
 
     def main(self):
-        ''' Executate method once.
-        '''
+        """ Execute method once.
+        """
 
-        # FIXME: grid utils not on control room workspaces
-        # upload GraceDB hardware injection event
-        #injupload.upload_gracedb_event(imminent_inj)
+        return "IDLE"
 
-        # FIXME: off until production
-        # set EPICs records for injection type and time
-        # ezca.write('TINJ_TYPE', 1)
-        # ezca.write('TINJ_START', imminent_inj.scheduled_time)
-        # ezca.write('TINJ_END', 0)
+class IDLE(GuardState):
+    """ The IDLE state continuously loops.
+    """
 
-        # call awgstream
-        log('Calling external command to perform injection')
-        cmd = map(str, [inj_exe, exc_channel, sample_rate, 
-               imminent_inj.waveform_path, imminent_inj.scale_factor])
-        retcode = injtools.make_external_call(cmd)
+    def run(self):
+        """ Execute method in a loop.
+        """
 
-        # FIXME: off until production
-        # set end time of injection
-        current_gps_time = gpstime.tconvert('now').gps()
-        # ezca.write('TINJ_END', current_gps_time)
+        # check if electromagnetic alert
+        exttrig_alert_time = check_exttrig_alert(exttrig_channel_name, exttrig_wait_time)
 
-        return 'IDLE'
+        # check schedule for imminent hardware injection
+        hwinj_list = read_schedule(schedule_path)
+        imminent_hwinj = check_imminent_injection(hwinj_list, imminent_wait_time)
 
-##################################################
-# EDGES
-##################################################
+        print exttrig_alert_time, imminent_hwinj
 
 # define directed edges that connect guardian states
 edges = (
-    ('INIT', 'DISABLED'),
-    ('DISABLED', 'IDLE'),
-    ('IDLE', 'CBC'),
+    ("ENABLED", "IDLE"),
 )
+
+
+
 
--- lib/injtools.py
+++ lib/injtools.py
@@ -1,59 +1,24 @@
-# -*- mode: python; tab-width: 4; indent-tabs-mode: nil -*-
-
-'''
-INJ tools module
-
-This defines how to read the schedule and determine if
-injections are to be performed.
-'''
-
-###############################################################################
-# IMPORTS
-###############################################################################
-
-import logging
 from gpstime import gpstime
 from subprocess import Popen, PIPE
 
-###############################################################################
-# VARIABLES
-###############################################################################
+class HardwareInjection(object):
+    """ A class representing a single hardware injection.
+    """
 
-# seconds before injection to call awgstream
-awgstream_time = 300
-
-# setup log
-log = logging.getLogger('INJ')
-
-###############################################################################
-# CLASSES
-###############################################################################
-
-class Injection(object):
-    ''' A class representing a single injection.
-    '''
-
-    def __init__(self, scheduled_time, inj_type,
+    def __init__(self, schedule_time, schedule_state, observation_mode,
                  scale_factor, waveform_path, metadata_path):
-        self.scheduled_time = float(scheduled_time)
-        self.inj_type = inj_type
+        self.schedule_time = float(schedule_time)
+        self.schedule_state = schedule_state
+        self.observation_mode = int(observation_mode)
         self.scale_factor = float(scale_factor)
         self.waveform_path = waveform_path
         self.metadata_path = metadata_path
 
-###############################################################################
-# FUNCTIONS
-###############################################################################
-
-def validate_schedule():
-    ''' Validate formatting of schedule file.
-
-    This function is not operational.
-    '''
-    pass
+    def __repr__(self):
+        return "<" + " ".join(map(str, [self.schedule_time, self.schedule_state])) + " HardwareInjection>"
 
 def read_schedule(schedule_path):
-    ''' Parses schedule and returns rows.
+    """ Parses schedule file.
 
     Parameters
     ----------
@@ -63,112 +28,100 @@ def read_schedule(schedule_path):
     Returns
     ----------
     inj_list: list
-        A list where each element is an Injection instance.
-    '''
+        A list where each element is an HardwareInjection instance.
+    """
 
-    # initialize empty list to store injections
-    inj_list = []
-
-    # get the current GPS time
-    current_gps_time = gpstime.tconvert('now').gps()
-
-    # open schedule file and add the injections to the list
-    with open(schedule_path, 'rb') as fp:
-        lines = fp.readlines()
-        for line in lines:
-            scheduled_time, inj_type, scale_factor, waveform_path, metadata_path = line.split()
-            inj = Injection(scheduled_time, inj_type, scale_factor, waveform_path, metadata_path)
-
-            # add injection to list if its in the future
-            if inj.scheduled_time-current_gps_time > 0:
-                inj_list.append(inj)
-
-    return inj_list
-
-def check_injections_enabled():
-    ''' Check that injections are enabled. This includes a few checks.
-
-    First check the EPICs record if injections are enabledo.
-
-    Then check if injections have been paused.
-
-    Last check if there is a recent electromagnetic alert.
-
-    Returns
-    ----------
-    bool
-        Returns True if injections are enabled. Returns False if injections are
-        not enabled.
-    '''
-
-    # check if injections enabled
-    tinj_enable = ezca.read('TINJ_ENABLE')
-    log.info('The value of %s is %f',
-        ezca.prefix+'TINJ_ENABLE', tinj_enable)
-
-    # check if injections paused
-    tinj_enable = ezca.read('TINJ_PAUSE')
-    log.info('The value of %s is %f',
-        ezca.prefix+'TINJ_PAUSE', tinj_enable)
-
-    # check if electromagnetic alert
-    exttrig_alert_time = ezca.read('EXTTRIG_ALERT_TIME')
-    log.info('The value of %s is %f',
-        ezca.prefix+'EXTTRIG_ALERT_TIME', exttrig_alert_time)
+    # initialize empty list to store HardwareInjection
+    hwinj_list = []
 
     # get the current GPS time
-    current_gps_time = gpstime.tconvert('now').gps()
+    current_gps_time = gpstime.tconvert("now").gps()
 
-    # if injections enabled and no electromangetic alert return True
-    if tinj_enable and (current_gps_time-exttrig_alert_time > awgstream_time):
-        return True
+    # read lines of schedule file
+    fp = open(schedule_path, "rb")
+    lines = fp.readlines()
+    fp.close()
 
-    # else return False
-    else:
-        return False
+    # loop over lines in schedule file
+    for line in lines:
 
-def check_injections_imminent(inj_list):
-    ''' Check for an imminent injection.
+        # get line in schedule as a list of strings
+        data = line.split()
+
+        # parse line
+        i = 0
+        schedule_time = float(data[i]); i += 1
+        schedule_state = data[i]; i += 1
+        observation_mode = int(data[i]); i+= 1
+        scale_factor = float(data[i]); i += 1
+        waveform_path = data[i]; i += 1
+        metadata_path = data[i]; i += 1
+
+        # add a new HardwareInjection to list if its in the future
+        if schedule_time - current_gps_time > 0:
+            hwinj = HardwareInjection(schedule_time, schedule_state,
+                                      observation_mode, scale_factor,
+                                      waveform_path, metadata_path)
+            hwinj_list.append(hwinj)
+
+    return hwinj_list
+
+def check_imminent_injection(hwinj_list, imminent_wait_time):
+    """ Find the most imminent hardware injection.
 
     Parameters
     ----------
-    inj_list: list
-        A list of Injection instances.
+    hwinj_list: list
+        A list of HardwareInjection instances.
 
     Retuns
     ----------
-    imminent_inj: Injection
-        An Injection instance is return if there is an imminent injection
-        found.
-    '''
+    imminent_hwinj: HardwareInjection
+        A HardwareInjection instance is return if there is an imminent
+        injection found.
+    """
 
     # get the current GPS time
-    current_gps_time = gpstime.tconvert('now').gps()
+    current_gps_time = gpstime.tconvert("now").gps()
 
     # find most imminent injection
-    if len(inj_list):
-        imminent_inj = min(inj_list, key=lambda x: x.scheduled_time-current_gps_time)
+    if len(hwinj_list):
+        imminent_hwinj = min(hwinj_list, key=lambda hwinj: hwinj.schedule_time-current_gps_time)
+        if imminent_hwinj.schedule_time-current_gps_time < imminent_wait_time \
+                      and imminent_hwinj.schedule_time-current_gps_time > 0:
+            return imminent_hwinj
+    return None
+
+def check_exttrig_alert(exttrig_channel_name, exttrig_wait_time):
+    """ Check if there is an external trigger alert.
+
+    Parameters
+    ----------
+    exttrig_channel_name: str
+        Name of the EPICs record channel to check for most recent alert time.
+    exttrig_wait_time: float
+        Amount of time to wait for an external alert.
+
+    Retuns
+    ----------
+    exttrig_alert_time: float
+        If external alert within wait period then return the GPS time of the alert.
+    """
+
+    # get the current GPS time
+    current_gps_time = gpstime.tconvert("now").gps()
+
+    # read EPICs record for most recent external trigger alert GPS time
+    exttrig_alert_time = ezca.read(exttrig_channel_name)
+
+    # if alert is within wait period then return the GPS time
+    if abs(current_gps_time - exttrig_alert_time) < exttrig_wait_time:
+        return exttrig_alert_time
     else:
         return None
-
-    # if most imminent injection is within time to call awgstream
-    # then return that injection
-    dt = imminent_inj.scheduled_time-current_gps_time
-    if dt <= awgstream_time and dt > 0:
-        return imminent_inj
-    else:
-        return None
-
-def check_detector_enabled():
-    ''' Check that the detector is in observation mode.
-
-    Function is not operational.
-    '''
-
-    return True
 
 def make_external_call(cmd_list, stdout=PIPE, stderr=PIPE, shell=False):
-    ''' Make an external call on the command line.
+    """ Make an external call on the command line.
 
     Parameters
     ----------
@@ -185,22 +138,23 @@ def make_external_call(cmd_list, stdout=PIPE, stderr=PIPE, shell=False):
     ----------
     process.returncode: int
         The exit code of the external call.
-    '''
+    """
 
     # run command
     cmd_list = map(str, cmd_list)
-    cmd_str = ' '.join(cmd_list)
-    log.info('Making external call: %s', cmd_str)
+    cmd_str = " ".join(cmd_list)
     process = Popen(cmd_list, shell=shell,
                   stdout=stdout, stderr=stderr)
 
     # get standard output and standard error
     stdout, stderr = process.communicate()
 
-    # if external call failed show stderr and stdout
-    if process.returncode:
-        log.debug('External call failed\n%s\n%s', stdout, stderr)
-    else:
-        log.info('External call successful')
-
     return process.returncode
+
+
+
+
+
+
+
+
