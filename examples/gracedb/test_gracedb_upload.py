a9b9f5082171146a2dfb3e4e454324ab6bd3a541
Christopher M. Biwer <christopher.biwer@ligo.org>
2016-02-08 11:39:46 -0500

    Update example.

--- examples/gracedb/test_gracedb_upload.py
+++ examples/gracedb/test_gracedb_upload.py
@@ -3,13 +3,16 @@
 from injtools import HardwareInjection
 from injupload import gracedb_upload_injection, gracedb_upload_message
 
+##### test 1
+
 # set some values for attributes
-schedule_time = 32.23
+schedule_time = 32.0
 schedule_state = "CBC"
 observation_mode = 0
 scale_factor = 1.0
 waveform_path = "H1-TEST-0-0.txt"
 metadata_path = "H1-TEST-0-0.xml"
+metadata_path = "None"
 
 # create a new HardwareInjection
 hwinj = HardwareInjection(schedule_time, schedule_state, observation_mode,
@@ -21,3 +24,20 @@ gracedb_id = gracedb_upload_injection(hwinj, ["H1"], group="Test")
 # append message to test entry
 message = "This is a test. There should be no injection in the data."
 gracedb_upload_message(gracedb_id, message)
+
+##### test 2
+
+# test case where no metadata_path
+hwinj.schedule_state = "Burst"
+hwinj.metadata_path = "None"
+
+# upload new test entry to GraceDB
+gracedb_id = gracedb_upload_injection(hwinj, ["H1"], group="Test")
+
+# append message to test entry
+message = "This is a test. There should be no injection in the data."
+gracedb_upload_message(gracedb_id, message)
+
+
+
+
